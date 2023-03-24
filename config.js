ad801f56540fe93042144df99b0d5ef316dd88cb
alexdavisiii <alex.iii.davis@gmail.com>
2023-03-23 23:26:53 -0400

    Setting things up!
    Commit includes basic index.html, script.js, and style.css files. Also
    contains config.js file and an images folder to be used later for
    styling purposes.
    
    Why am I doing this you ask? Well because I reached my number of API
    request limits on my other project, got a little frustrated, and am
    using this newer, better api which gives me 20000 a month. If I manage
    to hit that, I'll be very surprised.

---
+++ config.js
@@ -0,0 +1,3 @@
+const API_KEY = 'd5dad4cbf30741e59079df98b08e5023';
+
+export {API_KEY};
---
+++ script.js
@@ -0,0 +1,22 @@
+import { API_KEY } from "./config.js";
+
+const heading = document.getElementById('pcHead');
+const subHeading = document.getElementById('pcSubHead');
+const gamesList = document.getElementById('gamesList');
+
+console.log("hello");
+
+async function getGames(){
+    const games = await fetch(`https://api.rawg.io/api/platforms?key=${API_KEY}`, {
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then(response => response.json())
+        .then(jsonData => {
+            gamesList.innerHTML =`<p>${jsonData}</p>`
+            console.log(jsonData)})
+
+}
+
+getGames()
