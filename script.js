cd3a5710bc3a4e11488b9234f2fd8bf16f5ab753
alexdavisiii <alex.iii.davis@gmail.com>
2023-03-24 00:08:48 -0400

    added external API using fetch and async/await. Reads from console currently

--- script.js
+++ script.js
@@ -7,16 +7,11 @@ const gamesList = document.getElementById('gamesList');
 console.log("hello");
 
 async function getGames(){
-    const games = await fetch(`https://api.rawg.io/api/platforms?key=${API_KEY}`, {
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
+    const games = await fetch(`https://api.rawg.io/api/platforms?key=${API_KEY}`)
         .then(response => response.json())
         .then(jsonData => {
             gamesList.innerHTML =`<p>${jsonData}</p>`
             console.log(jsonData)})
-
 }
 
 getGames()
